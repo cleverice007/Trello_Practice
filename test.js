b3b00cb5f1cffac39a0b03816aeae5a5fbb5b318
cleverice007 <33803176+cleverice007@users.noreply.github.com>
2023-07-06 11:12:38 +0800

    modal demo

--- test.js
+++ test.js
@@ -1,34 +1,17 @@
-// 获取所需的DOM元素
-const cardContainer = document.getElementById('cardContainer');
-const addCardButton = document.getElementById('addCardButton');
+const card = document.getElementById('card1');
+const modal = document.getElementById('modal');
+const closeBtn = modal.querySelector('.close');
 
-// 添加新增卡片的功能
-addCardButton.addEventListener('click', function () {
-  const card = createCard();
-  cardContainer.appendChild(card);
-  card.addEventListener('click', function (event) {
-    if (event.target === card) {
-      const newTitle = prompt('请输入新的卡片标题：');
-      if (newTitle) {
-        card.querySelector('.card-title').textContent = newTitle;
-      }
-    }
-  });
+card.addEventListener('click', function() {
+  modal.style.display = 'block';
 });
 
-// 创建新卡片的函数
-function createCard() {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  const title = document.createElement('h3');
-  title.classList.add('card-title');
-  title.contentEditable = true;
-  title.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      title.blur();
-    }
-  });
-  card.appendChild(title);
-  return card;
-}
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
